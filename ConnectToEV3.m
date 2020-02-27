ac554616eee5a7dd88461cba21faeaa8c6fc1931
Jake Farabee <jfarabee@asu.edu>
2020-02-27 11:05:40 -0700

    Initial commit
    Created ConnectToEV3.m and MotorTest.m scripts

---
+++ ConnectToEV3.m
@@ -0,0 +1,4 @@
+%ConnectToEV3.m written by Jacob Farabee
+%2-27-2020  for FSE 100 w/ Dr. Chavez
+
+brick = ConnectBrick("12BRICKS");
---
+++ MotorTest.m
@@ -0,0 +1,10 @@
+%MotorTest.m written by Jacob Farabee
+%2/27/2020  for FSE 100 w/ Dr. Chavez
+
+%brick = ConnectBrick("12BRICKS");
+
+brick.MoveMotor('A', 50);
+brick.StopMotor('A');
+
+brick.MoveMotor('B', 50);
+brick.StopMotor('B');
